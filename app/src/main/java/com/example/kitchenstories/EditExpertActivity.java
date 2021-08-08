a915c55ac1f706393db618827185697647eec1da
Sahan Kumarasiri <kumarasirisahan@gmail.com>
2021-08-08 17:07:47 +0530

    First Design Commit

---
+++ app/src/main/java/com/example/kitchenstories/EditExpertActivity.java
@@ -0,0 +1,13 @@
+package com.example.kitchenstories;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditExpertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_expert);
+    }
+}
