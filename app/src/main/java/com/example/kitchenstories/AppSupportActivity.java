0a61636dfc54eda492cf15c2a574c8d09cd66065
ThamaliiNirmala <tnirmalaadasooriya@gmail.com>
2021-08-10 20:45:57 +0530

    First Community Design

---
+++ app/src/main/java/com/example/kitchenstories/AppSupportActivity.java
@@ -0,0 +1,14 @@
+package com.example.kitchenstories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppSupportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_support);
+    }
+}
