cb60630c3b022338961d8d9a00bb917377eff77e
SahanKumarasiri <kumarasirisahan@gmail.com>
2021-09-17 23:51:47 +0530

    Search Added!!1

--- app/src/main/java/com/example/kitchenstories/CreateProfile.java
+++ app/src/main/java/com/example/kitchenstories/CreateProfile.java
@@ -2,12 +2,35 @@ package com.example.kitchenstories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.database.ExpertDBHelper;
+import com.database.ProfileDBHelper;
+import com.database.ProfileModal;
+
+import java.util.List;
 
 public class CreateProfile extends AppCompatActivity {
+
     ImageView home , add , cart , expert , community , profile;
+    EditText name , email , phone , username;
+    Button create;
+    ImageButton search;
+
+    AlertDialog.Builder builder;
+
+    //Import DB
+    ProfileDBHelper DB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +42,16 @@ public class CreateProfile extends AppCompatActivity {
         expert = findViewById(R.id.expertS6);
         community = findViewById(R.id.communityS6);
         profile = findViewById(R.id.profileS6);
+
+        name = findViewById(R.id.profName);
+        email = findViewById(R.id.profEmail);
+        phone = findViewById(R.id.profPhone);
+        username = findViewById(R.id.profUsername);
+        create = findViewById(R.id.profCreateBtn);
+        search = findViewById(R.id.profileSearchBtn);
+
+        DB = new ProfileDBHelper(CreateProfile.this);
+        builder = new AlertDialog.Builder(this);
 
         home.setOnClickListener(v -> {
             Intent intent = new Intent(CreateProfile.this , KitchenStoriesActivity.class);
@@ -49,5 +82,58 @@ public class CreateProfile extends AppCompatActivity {
             Intent intent = new Intent(CreateProfile.this , CreateProfile.class);
             startActivity(intent);
         });
+        create.setOnClickListener(v -> {
+            String Name = name.getText().toString();
+            String Email = email.getText().toString();
+            String Phone = phone.getText().toString();
+            String Username = username.getText().toString();
+
+            if(Name.equals("")||Email.equals("")||Phone.equals("")||Username.equals(""))
+                Toast.makeText(CreateProfile.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+
+            //Uncomment the below code to Set the message and title from the strings.xml file
+            builder.setMessage("Do you want to create ?") .setTitle("Profile Creation");
+
+            //Setting message manually and performing action on button click
+            builder.setMessage("Do you want to create ?")
+                    .setCancelable(false)
+                    .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                            Toast.makeText(getApplicationContext(),"Registration Discarded",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            Boolean insert = DB.addInfo(Name, Email, Phone, Username);
+                            if (insert==true) {
+                                Toast.makeText(CreateProfile.this, "Added successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(CreateProfile.this, "Adding failed", Toast.LENGTH_SHORT).show();
+                            }
+                            name.setText("");
+                            email.setText("");
+                            phone.setText("");
+                            username.setText("");
+
+                            dialog.cancel();
+
+                        }
+                    });
+            //Creating dialog box
+            AlertDialog alert = builder.create();
+            //Setting the title manually
+            alert.setTitle("Profile Creation");
+            alert.show();
+
+        });
+
+        search.setOnClickListener(v -> {
+                Intent intent = new Intent(CreateProfile.this , SearchProfile.class);
+                startActivity(intent);
+        });
+
     }
 }
